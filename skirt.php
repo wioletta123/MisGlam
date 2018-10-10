d14b1b5b7b4a386d07fb9bdae9f26f26b6da8886
wioletta123 <42942255+wioletta123@users.noreply.github.com>
2018-10-10 18:14:25 +0100

    Create skirt.php

---
+++ skirt.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>MISSGLAM</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+        background-color:rgb(255, 191, 198);
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: rgb(255, 191, 198);
+      padding: 25px;
+    }
+    
+  .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
+
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none; 
+    }
+  }
+      $(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+     
+    container text-center{
+    width: 320px;
+    padding: 10px;
+    border: 5px solid gray;
+    margin: 0;
+      }
+      
+  input[type=text] {
+    width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('searchicon.png');
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+    width: 100%;
+}    
+      
+
+  </style>
+</head>
+<body>
+
+    <!-- nav bar -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">MISSGLAM</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+          <li><a href="http://localhost/newwebsite/tops.php">TOPS</a></li>
+        <li><a href="http://localhost/newwebsite/index.php">DRESSES</a></li>
+    <li class="active"><a href="http://localhost/newwebsite/skirts.php">SKIRTS</a></li>
+        <li><a href="http://localhost/newwebsite/access.php">ACCESSORIES</a></li>
+        </ul>
+      <ul class="nav navbar-nav navbar-right"><li><a href="http://localhost/newwebsite/php.php">Need help ? </a></li>
+      </ul>
+    </div>
+  </div>
+    <body>
+
+</body>
+</nav>
+
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="otherpic/new%20ba.jpeg" alt="Chania" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="otherpic/new2bae.jpeg" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="otherpic/banner_1481016963.jpg"alt="Flower" width="460" height="345">
+      </div>
+
+
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+  
+    
+<div class="container text-center">    
+  <h3>SKIRTS</h3><br>
+    <p>Nothing says seductive and covetable than a super stylish Missglam skirt. Whether you’re looking to turn some heads at the office or dress to impress on a first date, we’ve got exactly what you need. Our skirts come in a variety of looks and prints and we’ve got totally on-trend colours like oxblood and khaki. From flirty floral, to cool monochrome and everything in between, you can nail the catwalk trends like a true fashion pro. Our personal favs are the smokin’ hot bodycon styles and the 90s vibin’ a-line numbers. Flashin’ those pins has never been so fun; shop now before they run out!</p>
+    <div class="row">
+    <div class="col-sm-4">
+  <img src="https://media.missguided.com/s/missguided/S10000147_set/1/pink-faux-leather-button-paperbag-mini-skirt.jpg?$product-page__zoom--2x$" alt="Mesh Skirt with Glitter Embellishment - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S10000147_set/4/pink-faux-leather-button-paperbag-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S10000147_set/1/pink-faux-leather-button-paperbag-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+   <button class="btn" data-toggle="modal" data-target="#myModal">Bandage Bandeau Midi Dress with Halterneck</button> <br>
+        <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£25.00</a>
+
+        </div>
+        
+        
+
+    
+    <div class="col-sm-4">
+<img src="https://media.missguided.com/s/missguided/H9883035_set/2/white-seatbelt-detail-wrap-skort.jpg?$product-page__zoom--2x$" alt="Mini Bandage Skirt with Lattice Detail - Thumbnail." onmouseover="this.src='https://media.missguided.com/s/missguided/H9883035_set/3/white-seatbelt-detail-wrap-skort.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/H9883035_set/2/white-seatbelt-detail-wrap-skort.jpg?$product-page__zoom--2x$';" class="img-responsive">
+      
+        <button class="btn" data-toggle="modal" data-target="#myModal">Mini Bandage Skirt with Lattice Detail - Thumbnail</button> <br>
+         <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£25.00</a>
+        </div>
+        <div class="col-sm-4">
+    <img src="https://media.missguided.com/s/missguided/S9999863_set/1/pink-vinyl-utility-high-waisted-zip-up-mini-skirt.jpg?$product-page__zoom--2x$" alt="Pink High Waisted Sexy Vinyl Skirt  - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9999863_set/4/pink-vinyl-utility-high-waisted-zip-up-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9999863_set/1/pink-vinyl-utility-high-waisted-zip-up-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+           
+            <button class="btn" data-toggle="modal" data-target="#myModal">Pink High Waisted Sexy Vinyl Skirt</button> <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£15.00</a>
+        </div>
+    </div>
+        <div class="col-sm-4">
+      <img src="https://media.missguided.com/s/missguided/S9999940_set/1/yellow-check-super-high-waist-mini-skirt.jpg?$product-page__zoom--2x$" alt="Bandage Mini Skirt with Lace up detail on front - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9999940_set/2/yellow-check-super-high-waist-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9999940_set/1/yellow-check-super-high-waist-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+            
+               <button class="btn" data-toggle="modal" data-target="#myModal">Bandage Mini Skirt with Lace up detail on front</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£35.00</a>
+        </div>
+        <div class="col-sm-4">
+    <img src="https://media.missguided.com/s/missguided/G1802482_set/1/light-blue-ripped-a-line-denim-mini-skirt.jpg?$product-page__zoom--2x$" alt="Mesh Midi Skirt with Embellished Glitter Detail - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/G1802482_set/4/light-blue-ripped-a-line-denim-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/G1802482_set/1/light-blue-ripped-a-line-denim-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+       
+            <button class="btn" data-toggle="modal" data-target="#myModal">Mesh Midi Skirt with Embellished Glitter Detail</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£35.00</a>
+        </div>
+        <div class="col-sm-4">
+      <img src="https://media.missguided.com/s/missguided/S9999868_set/1/black-pleated-military-button-mini-skirt.jpg?$product-page__main--3x$" alt="Denim Midi Skirt with Front Zip Detail - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9999868_set/4/black-pleated-military-button-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9999868_set/1/black-pleated-military-button-mini-skirt.jpg?$product-page__main--3x$';" class="img-responsive">
+            
+            <button class="btn" data-toggle="modal" data-target="#myModal">Denim Midi Skirt with Front Zip Detail</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£26.00</a>
+        </div>
+        <div class="col-sm-4">
+       <img src="https://media.missguided.com/s/missguided/S9999864_set/1/black-vinyl-utility-high-waisted-zip-up-mini-skirt.jpg?$product-page__zoom--2x$" alt="Thigh split maxi skirt thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9999864_set/4/black-vinyl-utility-high-waisted-zip-up-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9999864_set/1/black-vinyl-utility-high-waisted-zip-up-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+           
+             <button class="btn" data-toggle="modal" data-target="#myModal">Thigh split maxi skirt</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£24.00</a>
+        </div>
+        <div class="col-sm-4">
+   <img src="https://media.missguided.com/s/missguided/S9995795_set/1/white-ribbed-lace-up-waist-mini-skirt.jpg?$product-page__zoom--2x$" alt="Red Vinyl Midi Skirt with Back Zip - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9995795_set/4/white-ribbed-lace-up-waist-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9995795_set/1/white-ribbed-lace-up-waist-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+           
+            <button class="btn" data-toggle="modal" data-target="#myModal">Red Vinyl Midi Skirt with Back Zip </button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£28.00</a>
+        </div>
+        <div class="col-sm-4">
+<img src="https://media.missguided.com/s/missguided/L4229325_set/1/orange-mesh-floral-side-split-maxi-skirt.jpg?$product-page__zoom--2x$" alt="485-serenity-skirt-thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/L4229325_set/2/orange-mesh-floral-side-split-maxi-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/L4229325_set/1/orange-mesh-floral-side-split-maxi-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+        <button class="btn" data-toggle="modal" data-target="#myModal">serenity skirt</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£25.00</a>
+        </div>
+        <div class="col-sm-4">
+<img src="https://media.missguided.com/s/missguided/S9999967_set/1/white-lemon-print-mesh-gathered-skirt.jpg?$product-page__zoom--2x$" alt="Womens Sexy Double Layered Bodycon Midi Skirt - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9999967_set/4/white-lemon-print-mesh-gathered-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9999967_set/1/white-lemon-print-mesh-gathered-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+            
+            <button class="btn" data-toggle="modal" data-target="#myModal">Womens Sexy Double Layered Bodycon Midi Skirt</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£35.00</a>
+        </div>
+        <div class="col-sm-4">
+       <img src="https://media.missguided.com/s/missguided/S9998250_set/1/peace--love-nude-embellished-sheer-skirt.jpg?$product-page__zoom--2x$" alt="Bandage Mini Skirt with Criss Cross Detail and zip up back - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9998250_set/4/peace--love-nude-embellished-sheer-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9998250_set/1/peace--love-nude-embellished-sheer-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+           
+            <button class="btn" data-toggle="modal" data-target="#myModal">Bandage Mini Skirt with Criss Cross</button>
+            <br>
+            <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£29.00</a>
+        </div>
+        <div class="col-sm-4">
+      <img src="https://media.missguided.com/s/missguided/S9999448_set/1/yellow-leather-mini-skirt.jpg?$product-page__zoom--2x$" alt="Leatherette Midi Skirt with Back Zip - Thumbnail" onmouseover="this.src='https://media.missguided.com/s/missguided/S9999448_set/4/yellow-leather-mini-skirt.jpg?$product-page__zoom--2x$';" onmouseout="this.src='https://media.missguided.com/s/missguided/S9999448_set/1/yellow-leather-mini-skirt.jpg?$product-page__zoom--2x$';" class="img-responsive">
+        </div>
+        <button class="btn" data-toggle="modal" data-target="#myModal">Leatherette Midi Skirt </button>
+        <br>
+        <a href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">£28.00</a>
+        </div> 
+</div><br>
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content
+   pops up when someone clicks on the name of the items-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">×</button>
+          <h4><span class="glyphicon glyphicon-lock"></span> SKIRTS</h4>
+        </div>
+        <div class="modal-body">
+          <form role="form">
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-shopping-cart"></span> Quantity</label>
+              <input type="number" class="form-control" id="psw" placeholder="How many?">
+            </div>
+              <label for="size">size</label>
+    <select id="size" name="size">
+      <option value="6">6</option>
+      <option value="8">8</option>
+      <option value="10">10</option>
+        <option value="10">12</option>
+        <option value="10">14</option>
+        <option value="10">16</option>
+    </select>
+          
+              <button type="submit" class="btn btn-block">Add to your Basket
+                <span class="glyphicon glyphicon-ok"></span>
+              </button>
+          </form>
+         </div>
+          
+                <div class="modal-footer">
+          <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal">
+            <span class="glyphicon glyphicon-remove"></span> Cancel
+          </button>
+          <p>Need <a href="http://localhost/newwebsite/php.php">help?</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+    <!-- footer-->
+ <footer class="container-fluid text-center">
+  <p>© MISSGLAM LTD // OFFICIAL BABE UNIFORM</p>  
+    
+
+    </footer>
+
+</body>
+</html>
